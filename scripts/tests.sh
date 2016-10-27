15a9d427b9b8864c4dc19ba31734dea4efcb6b24
Steve Oliver <steve@circatree.com>
2016-10-27 00:26:51 -0700

    Fix test locations.

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -5,9 +5,9 @@ sudo chown -R 82:82 .
 docker-compose up -d mariadb
 docker-compose up -d nginx
 
-docker-compose run --user 82 php vendor/bin/phpunit -c web/core web/core/tests/Drupal/Tests/Core/Password/PasswordHashingTest.php
-docker-compose run --user 82 php vendor/bin/phpunit -c web/core web/core/tests/Drupal/KernelTests/Component/Utility/SafeMarkupKernelTest.php
-docker-compose run --user 82 php vendor/bin/phpunit -c web/core web/core/tests/Drupal/FunctionalTests/Breadcrumb/Breadcrumb404Test.php
+docker-compose run --user 82 php vendor/bin/phpunit -c core core/tests/Drupal/Tests/Core/Password/PasswordHashingTest.php
+docker-compose run --user 82 php vendor/bin/phpunit -c core core/tests/Drupal/KernelTests/Component/Utility/SafeMarkupKernelTest.php
+docker-compose run --user 82 php vendor/bin/phpunit -c core core/tests/Drupal/FunctionalTests/Breadcrumb/Breadcrumb404Test.php
 
 sudo chown -R jenkins:jenkins .
 
